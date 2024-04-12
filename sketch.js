e7df64f86a2f93b32dce87b9f3739fe9c1eaebe3
Ancantana <132713654+Ancantana@users.noreply.github.com>
2024-04-11 20:06:39 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,8 @@ let selectedImage = null;
 let draggedImage = null;
 let dragOffsetX, dragOffsetY;
 let resizeDirection = '';
+let video;
+let videoStream;
 let videoVisible = false;
 
 function preload() {
@@ -34,6 +36,10 @@ function setup() {
   snapButton.hide();
 
   loadGalleryImages();
+
+  video = createCapture(VIDEO);
+  video.size(197, 197);
+  video.hide();
 }
 
 function draw() {
@@ -66,17 +72,22 @@ function draw() {
     textSize(12);
     text('Resize', x + w - 37.5, y + h - 21);
   }
+
+  // Display video feed
+  if (videoVisible) {
+    image(video, 0, 0, 197, 197);
+  }
 }
 
 function loadGalleryImages() {
   let imgUrls = [
-    'https://ancantana.github.io/heaventestin/original_77f8f96b25a80928f3f31b83d967fd2d.png',
-    'https://ancantana.github.io/heaventestin/original_052e51d86c2267360a21d5e9bfb41935.png',
-    'https://ancantana.github.io/heaventestin/original_29ab04b0486cb7c8845a663e33adfb13.png',
-    'https://ancantana.github.io/heaventestin/original_372f6230eb41e5f365737fcda89f50c3.png',
-    'https://ancantana.github.io/heaventestin/original_bd74eb6f0884a30cfb2d0af7943a14f9.png',
-    'https://ancantana.github.io/heaventestin/original_c14396968eb286bfacdd00e9a0577937.png',
-    'https://ancantana.github.io/heaventestin/original_c3c5f8eec85f4387b6a842cef208d51b.png'
+    'https://ancantana.github.io/heaven/original_77f8f96b25a80928f3f31b83d967fd2d.png',
+    'https://ancantana.github.io/heaven/original_052e51d86c2267360a21d5e9bfb41935.png',
+    'https://ancantana.github.io/heaven/original_29ab04b0486cb7c8845a663e33adfb13.png',
+    'https://ancantana.github.io/heaven/original_372f6230eb41e5f365737fcda89f50c3.png',
+    'https://ancantana.github.io/heaven/original_bd74eb6f0884a30cfb2d0af7943a14f9.png',
+    'https://ancantana.github.io/heaven/original_c14396968eb286bfacdd00e9a0577937.png',
+    'https://ancantana.github.io/heaven/original_c3c5f8eec85f4387b6a842cef208d51b.png'
   ];
 
   imgUrls.forEach((url, i) => {
@@ -88,6 +99,27 @@ function loadGalleryImages() {
 
 function toggleGallery() {
   galleryVisible = !galleryVisible;
+}
+
+function startVideo() {
+  if (!videoStream) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(stream => {
+        videoStream = stream;
+        video.elt.srcObject = stream;
+        video.play();
+      })
+      .catch(error => {
+        console.error('Error accessing camera:', error);
+      });
+  }
+}
+
+function stopVideo() {
+  if (videoStream) {
+    videoStream.getTracks().forEach(track => track.stop());
+    videoStream = null;
+  }
 }
 
 function toggleVideoAndText() {
@@ -103,30 +135,9 @@ function toggleVideoAndText() {
   }
 }
 
-function startVideo() {
-  const videoElement = document.getElementById('video-element');
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-      videoElement.srcObject = stream;
-    })
-    .catch(error => {
-      console.error('Error accessing camera:', error);
-    });
-}
-
-function stopVideo() {
-  const videoElement = document.getElementById('video-element');
-  const stream = videoElement.srcObject;
-  if (stream) {
-    stream.getTracks().forEach(track => track.stop());
-    videoElement.srcObject = null;
-  }
-}
-
 function takeSnapshot() {
-  const videoElement = document.getElementById('video-element');
-  const snapshot = createImage(197, 197);
-  snapshot.drawingContext.drawImage(videoElement, 0, 0, 197, 197);
+  let snapshot = createImage(197, 197);
+  snapshot.copy(video, 0, 0, 197, 197, 0, 0, 197, 197);
   let snapshotX = width / 2 - 98.5;
   let snapshotY = height / 2 - 98.5;
   draggedImages.push({ img: snapshot, x: snapshotX, y: snapshotY, w: 197, h: 197, selected: false });
